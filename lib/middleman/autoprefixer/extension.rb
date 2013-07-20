44c8507c4aa84b1c36c19d05e80d4721f908e6e9
Dominik Porada <dominik@porada.co>
2013-07-20 20:59:28 +0200

    Implement `browsers` option

--- lib/middleman/autoprefixer/extension.rb
+++ lib/middleman/autoprefixer/extension.rb
@@ -2,10 +2,18 @@ require 'autoprefixer-rails'
 
 module Middleman
   module Autoprefixer
+    class Options < OpenStruct; end
+
     class << self
       def registered(app, options_hash={}, &block)
+        @@options = Options.new(options_hash)
+        yield @@options if block_given?
+
+        browsers = Array(@@options.browsers)
+        browsers = nil if browsers.empty?
+
         app.after_configuration do
-          AutoprefixerRails.install(sprockets)
+          AutoprefixerRails.install(sprockets, browsers)
         end
       end
       alias :included :registered
