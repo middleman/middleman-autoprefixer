ddcd9c17b9b37169f6edd5af4804574f5dbd6bc0
Dominik Porada <dominik@porada.co>
2014-03-01 23:15:10 +0100

    Ignore exceptions only caused when parsing CSS

--- lib/middleman-autoprefixer/processor.rb
+++ lib/middleman-autoprefixer/processor.rb
@@ -3,10 +3,20 @@ require 'autoprefixer-rails'
 module Middleman
   module Autoprefixer
     class Processor < AutoprefixerRails::Processor
-      def configure(sprockets_environment)
-        sprockets_environment.register_postprocessor 'text/css', :autoprefixer do |_, data|
-          process(data).css rescue data
+      def postprocess(context, content)
+        begin
+          process(content).css
+        rescue ExecJS::ProgramError => error
+          if error.message =~ /Can't parse CSS/
+            content
+          else
+            raise error
+          end
         end
+      end
+
+      def configure(sprockets_env)
+        sprockets_env.register_postprocessor 'text/css', :autoprefixer, &method(:postprocess)
       end
     end
   end
